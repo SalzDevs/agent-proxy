84a77116537d5571143a6c43fc6ed28169b7d850
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-08 18:34:26 +0100

    add configurable timeout settings

--- config.go
+++ config.go
@@ -1,8 +1,20 @@
 package groxy
 
+import "time"
+
 // Config contains settings used to create a Proxy.
 type Config struct {
 	// Addr is the TCP address the proxy listens on, such as "127.0.0.1:8080".
 	Addr string
+	Timeouts Timeouts
+}
+
+type Timeouts struct {
+	Dial time.Duration
+	TLSHandshake time.Duration
+	ResponseHeader time.Duration
+	IdleConn time.Duration
+	ReadHeader time.Duration
+	Idle time.Duration
 }
 
